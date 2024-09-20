92b8d05b9c8d6da6f3c73a73d04bda5d02b0e4d8
JesserHamdaoui <107654958+JesserHamdaoui@users.noreply.github.com>
2024-09-20 13:29:55 +0100

    feat: add configuration with lowlevel api

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from cvat_sdk import Config, Client, models
-from cvat_sdk.core.proxies.tasks import ResourceType, Task
+from cvat_sdk.api_client import Configuration, ApiClient, models, apis, exceptions
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-config = Config()
 
-# print(os.getenv("CVAT_USERNAME"))
-# print(os.getenv("CVAT_PASSWORD"))
+configuration = Configuration(
+    host="http://localhost",
+    username=os.getenv("CVAT_USERNAME"),
+    password=os.getenv("CVAT_PASSWORD"),
+)
 
-with Client("https://localhost", config=config) as client:
-    client.login((os.getenv("CVAT_USERNAME"), os.getenv("CVAT_PASSWORD")))
-    print(client)
+with ApiClient(configuration) as api_client:
+    tasks_api = apis.TasksApi(api_client)
+    tasks = tasks_api.list()
+    print(tasks)
