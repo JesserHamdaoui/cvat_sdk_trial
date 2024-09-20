95fe8c4ca9962cf5474654887ec000307141c167
JesserHamdaoui <107654958+JesserHamdaoui@users.noreply.github.com>
2024-09-20 12:49:11 +0100

    feat: try configuration with high level api

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from cvat_sdk import Configuration, ApiClient, models, apis, exceptions
+from cvat_sdk import Config, Client, models
+from cvat_sdk.core.proxies.tasks import ResourceType, Task
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
+config = Config()
 
-configuration = Configuration(
-    host="http://localhost",
-    username= os.getenv('CVAT_USERNAME'),
-    password=os.getenv('CVAT_PASSWORD')
-)
+# print(os.getenv("CVAT_USERNAME"))
+# print(os.getenv("CVAT_PASSWORD"))
 
+with Client("https://localhost", config=config) as client:
+    client.login((os.getenv("CVAT_USERNAME"), os.getenv("CVAT_PASSWORD")))
+    print(client)
